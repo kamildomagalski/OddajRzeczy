73584c0a6f27266221d601a706802a6fdc3986d5
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-07 15:28:49 +0200

    MainSection styling in Sass

--- src/components/Homepage/MainSection.js
+++ src/components/Homepage/MainSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import decoration from '../../assets/Decoration.svg'
 
 function MainSection() {
   return (
     <section className={'main'}>
       <header className={'header'}>
         <div className={'logIn'}>
-          <button className={'btn btn-small'}>Zaluguj</button>
+          <button className={'btn btn-small'}>Zaloguj</button>
           <button className={'btn btn-small'}>Załóż konto</button>
         </div>
         <ul className={'menu'}>
@@ -18,9 +18,9 @@ function MainSection() {
         </ul>
       </header>
       <div className={'content'}>
-        <h2>Zacznij pomagać!</h2>
-        <h2>Oddaj niechciane rzeczy w zaufane ręce</h2>
-        <img className={'ornament'}/>
+        <h2 className={'content__title'}>Zacznij pomagać!</h2>
+        <h2 className={'content__title'}>Oddaj niechciane rzeczy w zaufane ręce</h2>
+        <img className={'decoration'} src={decoration} alt={'decoration'}/>
         <div className={'content__buttons'}>
           <button className={'btn btn-big'}>Oddaj Rzeczy</button>
           <button className={'btn btn-big'}>Zorganizuj Zbiórkę</button>
