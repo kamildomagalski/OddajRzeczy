64b887341350533abaa131ec6940402a4ec05f58
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-08 17:13:16 +0200

    add Info Section

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MainSection from "./MainSection";
-
+import InfoSection from "./InfoSection";
 
 function Home() {
  return (
   <>
   <MainSection/>
+  <InfoSection/>
   </>
  );
 }
