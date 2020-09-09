5a4c3025aff94c4582cd4ca5129437544096e10f
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-09 07:51:04 +0200

    added Steps section with RWD styling

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MainSection from "./MainSection";
 import InfoSection from "./InfoSection";
+import StepsSection from "./StepsSection";
+
+
 
 function Home() {
  return (
   <>
   <MainSection/>
   <InfoSection/>
+  <StepsSection/>
   </>
  );
 }
