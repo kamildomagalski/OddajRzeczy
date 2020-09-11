e9bcfb0018d7373cf283fb2c49c3c6228bfc6fb7
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-11 13:11:00 +0200

    add signUp Form component with Firebase Auth configuration

--- src/components/Application/SignUp.js
+++ src/components/Application/SignUp.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import AppHeader from "./AppHeader";
 import decoration from "../../assets/Decoration.svg";
-import {Link} from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import {FirebaseContext} from "../Firebase";
+
+
 
 function SignUp() {
+ 
+ 
  return (
    <section className={'signup'}>
      <AppHeader/>
      <div className={'signup__content'}>
        <h1 className={'signup__title'}>Załóż konto</h1>
        <img className={'decoration'} src={decoration} alt={'decoration'}/>
-       <div className={'signup__form'}>
-         <div className={'signup__wrapper'}>
-           <p className={'signup__subtitle'}>Email</p>
-           <input className={'signup__input signup__email'}/>
-         </div>
-         <div className={'signup__wrapper'}>
-           <p className={'signup__subtitle'}>Hasło</p>
-           <input type={'password'} className={'login__input signup__password'}/>
-         </div>
-         <div className={'signup__wrapper'}>
-           <p className={'signup__subtitle'}>Powtórz hasło</p>
-           <input type={'password'} className={'signup__input signup__password'}/>
-         </div>
-       </div>
-       <div className={'signup__buttons'}>
-         <Link to={'/login'} className={'btn btn-small btn-noBorder'}>Zaloguj się</Link>
-         <button className={'btn btn-small btn-border'}>Załóż konto</button>
-       </div>
+       <FirebaseContext.Consumer>
+         {firebase => <SignUpForm firebase={firebase}/>}
+       </FirebaseContext.Consumer>
      </div>
    </section>
  );
---
+++ src/components/Application/SignUpForm.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+
+
+function SignUpForm({firebase}) {
+  
+  const [signUpState, setSignUpState] = useState({
+    email: '',
+    password1: '',
+    password2: '',
+    error: null
+  });
+  
+  
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setSignUpState(prevState => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+  
+  const handleSubmit = (event) =>{
+    const {email, password1}=signUpState;
+    
+    event.preventDefault();
+    firebase
+      .doCreateUserWithEmailAndPassword(email, password1)
+      .then(authUser => {
+        setSignUpState({...signUpState});
+      })
+      .catch(error => {
+        setSignUpState({
+          ...signUpState,
+          error
+        });
+      })
+  }
+  
+  const isInvalid=
+    signUpState.password1 !== signUpState.password2 ||
+    signUpState.password1.length < 6 ||
+    signUpState.email === '/\S+@\S+\.\S+/';
+  return (
+    <>
+      <form className={'signup__form'}>
+        <div className={'signup__wrapper'}>
+          <p className={'signup__subtitle'}>Email</p>
+          <input name={'email'}
+                 value={signUpState.email}
+                 onChange={handleChange}
+                 type={'text'}
+                 className={'signup__input signup__email'}/>
+        </div>
+        <div className={'signup__wrapper'}>
+          <p className={'signup__subtitle'}>Hasło</p>
+          <input name={'password1'}
+                 value={signUpState.password1}
+                 onChange={handleChange}
+                 type={'password'}
+                 className={'login__input signup__password'}/>
+        </div>
+        <div className={'signup__wrapper'}>
+          <p className={'signup__subtitle'}>Powtórz hasło</p>
+          <input name={'password2'}
+                 value={signUpState.password2}
+                 onChange={handleChange}
+                 type={'password'}
+                 className={'signup__input signup__password'}/>
+        </div>
+        {signUpState.error && <p>{signUpState.error.message}</p>}
+      </form>
+      <div className={'signup__buttons'}>
+        <Link to={'/login'} className={'btn btn-small btn-noBorder'}>Zaloguj się</Link>
+        <button type={'submit'} onClick={handleSubmit} disabled={isInvalid} className={'btn btn-small btn-border'}>Załóż konto</button>
+      </div>
+    </>
+  );
+}
+
+export default SignUpForm;
