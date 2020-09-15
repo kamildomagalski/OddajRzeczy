d3e10ea51e5984be98d023de7981a3deb7cf2c95
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-15 17:13:35 +0200

    added logic to Homepage form; check validation before fetch, throw errors and success message

--- src/components/Homepage/ContactSection.js
+++ src/components/Homepage/ContactSection.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import decoration from '../../assets/Decoration.svg'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import ContactForm from "./ContactForm";
 
 function ContactSection() {
+  const [isSent, setIsSent]= useState(false)
+  
+  const handleSend = (value) => {
+    setIsSent(value)
+  }
+  
+  const MessageSuccess= () =>{
+    return (
+      <div className={'messageSuccess'}>
+        <p className={'messageSuccess__text'}>Wiadomość została wysłana!</p>
+        <p className={'messageSuccess__text'}>Wkrótce sie z Tobą skontaktujemy.</p>
+      </div>
+    )
+  }
+  
   return (
     <section className={'contact'} id={'contact'}>
       <div className={'backgroundFilter'}>
         <div className={'contact__section'}>
           <h2 className={'contact__title'}>Skontaktuj się z nami</h2>
           <img className={'decoration'} src={decoration} alt={'decoration'}/>
-          <form className={'contact__form'}>
-            <div className={'contact__details'}>
-              <div className={'contact__wrapper'}>
-                <h3 className={'contact__subtitle'}>Wpisz swoje imię</h3>
-                <input className={'contact__input contact__name'} placeholder={'Krzysztof'}/>
-              </div>
-              <div className={'contact__wrapper'}>
-                <h3 className={'contact__subtitle'}>Wpisz swój email</h3>
-                <input className={'contact__input contact__email'} placeholder={'abc@xyz.pl'}/>
-              </div>
-            </div>
-            
-            <div className={'contact__wrapper'}>
-              <h3 className={'contact__subtitle'}>Wpisz swoją wiadomosć</h3>
-              <textarea className={'contact__input contact__message '}
-                        placeholder={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-                        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
-                        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ' +
-                        'nisi ut aliquip ex ea commodo consequat.'}/>
-            </div>
-            
-            <button className={'btn btn-small btn-contact'}>Wiślij</button>
-          </form>
+          {isSent? <MessageSuccess/> : null}
+          <ContactForm handleSend={handleSend}/>
         </div>
         <footer className={'footer'}>
           <p>Copyright by Coders Lab</p>
