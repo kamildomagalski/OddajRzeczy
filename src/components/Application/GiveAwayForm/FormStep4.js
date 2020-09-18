554f2e0715e9d81a9f6ee2574c7c4f50ea9ffcb8
Kamil Domagalski <kdbrasi360@gmail.com>
2020-09-18 10:06:31 +0200

    added formStep 4 with styles

--- src/components/Application/GiveAwayForm/FormStep4.js
+++ src/components/Application/GiveAwayForm/FormStep4.js
@@ -17,7 +17,7 @@ function FormStep4() {
     })
   }
   const handleCourierChange = (event) => {
-    const { name, value } = event.target;
+    const {name, value} = event.target;
     setCourierData(prevState => {
       return {
         ...prevState,
@@ -46,66 +46,91 @@ function FormStep4() {
   if (formData.step !== 4) return null
   
   return (
-    <>
-      <h1 className={'appPulpit__title'}>Podaj adres i termin odbioru:</h1>
-      <form className={'appPulpit__form'}>
-        <h2>Termin odbioru:</h2>
-        <label>
-          Ulica:
-          <input name={'street'}
-                 value={postData.street}
-                 onChange={handlePostChange}
-                 type={'text'}/>
-        </label>
-        <label>
-          Miasto:
-          <input name={'city'}
-                 value={postData.city}
-                 onChange={handlePostChange}
-                 type={'text'}/>
-        </label>
-        <label>
-          Kod pocztowy:
-          <input name={'postcode'}
-                 value={postData.postcode}
-                 onChange={handlePostChange}
-                 type={'number'}/>
-        </label>
-        <label>
-          Numer telefonu:
-          <input name={'phone'}
-                 value={postData.phone}
-                 onChange={handlePostChange}
-                 type={'number'}/>
-        </label>
-        <h2>Termin odbioru</h2>
-        <label>
-          Data:
-          <input name={'date'}
-                 value={courierData.date}
-                 onChange={handleCourierChange}
-                 type={'number'}/>
-        </label>
-        <label>
-          Godzina:
-          <input name={'time'}
-                 value={courierData.time}
-                 onChange={handleCourierChange}
-                 type={'text'}/>
-        </label>
-        <label>
-          Uwagi:
-          <input name={'note'}
-                 value={courierData.note}
-                 onChange={handleCourierChange}
-                 type={'text'}/>
-        </label>
-        
-        <button onClick={handlePrevPage} type={"button"} className={'btn btn-small'}>Wstecz</button>
-        <button onClick={handleSubmit} type={"submit"} className={'btn btn-small'}>Dalej</button>
-      </form>
-    
-    </>
+    <section className={'formStep'}>
+      <div className={'warning'}>
+        <div className={'container'}>
+          <h2 className={'warning__title'}>Ważne!</h2>
+          <p className={'warning__text'}>Podaj adres oraz termin odbioru rzeczy.</p>
+        </div>
+      </div>
+      <div className={'container'}>
+        <p className={'formStep__counter'}>Krok {formData.step}/4</p>
+        <h1 className={'formStep__title'}>Podaj adres oraz termin odbioru rzecz przez kuriera</h1>
+        <form className={'formStep__form'}>
+          <div className={'formStep__wrapper'}>
+            <div className={'formStep__column'}>
+              <h2 className={'formStep__subtitle'}>Adres odbioru:</h2>
+              <label className={'formStep__label'}>
+                Ulica
+                <input name={'street'}
+                       value={postData.street}
+                       onChange={handlePostChange}
+                       type={'text'}
+                       className={'formStep__input'}/>
+              </label>
+              <label className={'formStep__label'}>
+                Miasto
+                <input name={'city'}
+                       value={postData.city}
+                       onChange={handlePostChange}
+                       type={'text'}
+                       className={'formStep__input'}/>
+              </label>
+              <label className={'formStep__label'}>
+                Kod pocztowy
+                <input name={'postcode'}
+                       value={postData.postcode}
+                       onChange={handlePostChange}
+                       type={'number'}
+                       className={'formStep__input'}/>
+              </label>
+              <label className={'formStep__label'}>
+                Numer telefonu
+                <input name={'phone'}
+                       value={postData.phone}
+                       onChange={handlePostChange}
+                       type={'number'}
+                       className={'formStep__input'}/>
+              </label>
+            </div>
+            <div className={'formStep__column'}>
+              <h2 className={'formStep__subtitle'}>Termin odbioru</h2>
+              <label className={'formStep__label'}>
+                Data
+                <input name={'date'}
+                       value={courierData.date}
+                       onChange={handleCourierChange}
+                       type={'number'}
+                       className={'formStep__input'}/>
+              </label>
+              <label className={'formStep__label'}>
+                Godzina
+                <input name={'time'}
+                       value={courierData.time}
+                       onChange={handleCourierChange}
+                       type={'text'}
+                       className={'formStep__input'}/>
+              </label>
+              <label className={'formStep__label'}>
+                Uwagi
+                <input name={'note'}
+                       value={courierData.note}
+                       onChange={handleCourierChange}
+                       type={'text'}
+                       className={'formStep__input'}/>
+              </label>
+            </div>
+          </div>
+          <div className={'formStep__buttons'}>
+            <button onClick={handlePrevPage} type={"button"}
+                    className={'btn btn-small btn-border formStep__btn'}>Wstecz
+            </button>
+            <button onClick={handleSubmit} type={"submit"} className={'btn btn-small btn-border formStep__btn'}>Dalej
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
